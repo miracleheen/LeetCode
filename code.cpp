42640d8910b8823d129156c8e1ae11e0190ac991
Miracle <miracleheen99@gmail.com>
2024-02-03 16:16:31 +0300

    task

---
+++ code.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> index;
+        for(int i = 0;index<nums.size(); ++i){
+            if(index.count(target-nums[i])){
+                return {index[target-nums[i]],i};
+            }
+            index[nums[i]] = i;
+        }
+        return {0,0};
+    }
+};
